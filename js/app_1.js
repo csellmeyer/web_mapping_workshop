bf6743ad4f3ff2a4fef78f618a1b4bc2b1bedbb4
csellmeyer <charlessellmeyer@yahoo.com>
2014-11-13 13:52:44 -0600

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////
 // Enter your mapbox map id here to reference it for the base layer
 
-var mapId = 'will-breitkreutz.k6fj4l3f'; //<- this references the ugly green map that I made
-var token = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw'; //<- this is my token, use yours.
+var mapId = 'csellmeyer.k7b8ljme'; //<- this references the ugly green map that I made
+var token = 'pk.eyJ1IjoiY3NlbGxtZXllciIsImEiOiJ3cUJTdDJNIn0.yw7Q8RtfPgHyI9WR7DdYtw'; //<- this is my token, use yours.
 
 //Create the map object with your mapId and token
 L.mapbox.accessToken = token;
